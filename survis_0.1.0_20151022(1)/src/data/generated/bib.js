5a6a21dfd2d77c8dda5d2a5f2e90225c191681f5
“Mathew <psxma18@exmail.nottingham.ac.uk>
2024-05-17 14:13:26 +0100

    first

---
+++ survis_0.1.0_20151022(1)/src/data/generated/bib.js
@@ -0,0 +1,107 @@
+﻿define({ entries : {
+    "": {
+        "type": "article"
+    },
+    "10283446": {
+        "author": "Su, Jau-Lang and Yeh, Chia-Cheng and Alkhaleefah, Mohammad and Chang, Lena and Chang, Yang-Lang",
+        "booktitle": "IGARSS 2023 - 2023 IEEE International Geoscience and Remote Sensing Symposium",
+        "doi": "10.1109/IGARSS52108.2023.10283446",
+        "keywords": "Deep learning;Training;Image segmentation;Architecture;Buildings;Feature extraction;Satellite images;Climate change;Deep learning;convolutional neural networks;telemetry satellite imagery;building segmentation;disaster damage assessment",
+        "number": "",
+        "pages": "285-288",
+        "title": "A Deep Convolutional Neural Network for Building Damage Evaluation from Satellite Images",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2023"
+    },
+    "9412295": {
+        "author": "Gupta, Rohit and Shah, Mubarak",
+        "booktitle": "2020 25th International Conference on Pattern Recognition (ICPR)",
+        "doi": "10.1109/ICPR48806.2021.9412295",
+        "keywords": "Location awareness;Image segmentation;Computer vision;Satellites;Head;Architecture;Tiles",
+        "number": "",
+        "pages": "4405-4411",
+        "title": "RescueNet: Joint Building Segmentation and Damage Assessment from Satellite Imagery",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9442902": {
+        "author": "Shen, Yu and Zhu, Sijie and Yang, Taojiannan and Chen, Chen and Pan, Delu and Chen, Jianyu and Xiao, Liang and Du, Qian",
+        "doi": "10.1109/TGRS.2021.3080580",
+        "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+        "keywords": "Buildings;Image segmentation;Satellites;Feature extraction;Task analysis;Neural networks;Remote sensing;Building damage assessment;convolutional neural network (CNN);cross-directional attention (CDA);CutMix;multiscale feature fusion (MFF);satellite image",
+        "number": "",
+        "pages": "1-14",
+        "title": "BDANet: Multiscale Convolutional Neural Network With Cross-Directional Attention for Building Damage Assessment From Satellite Images",
+        "type": "ARTICLE",
+        "volume": "60",
+        "year": "2022"
+    },
+    "9883139": {
+        "author": "Chen, Hongruixuan and Nemni, Edoardo and Vallecorsa, Sofia and Li, Xi and Wu, Chen and Bromley, Lars",
+        "booktitle": "IGARSS 2022 - 2022 IEEE International Geoscience and Remote Sensing Symposium",
+        "doi": "10.1109/IGARSS46834.2022.9883139",
+        "keywords": "Computer vision;Architecture;Buildings;Computer architecture;Transformers;Feature extraction;Decoding;Transformer;building damage assessment;multi-task learning;deep learning;multitemporal images",
+        "number": "",
+        "pages": "1600-1603",
+        "title": "Dual-Tasks Siamese Transformer Framework for Building Damage Assessment",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2022"
+    },
+    "9884915": {
+        "author": "Pang, Lei and Zhang, Fengli and Li, Lu and Huang, Qiqi and Jiao, Yanan and Shao, Yun",
+        "booktitle": "IGARSS 2022 - 2022 IEEE International Geoscience and Remote Sensing Symposium",
+        "doi": "10.1109/IGARSS46834.2022.9884915",
+        "keywords": "Training;Open Access;Buildings;Semantics;Geoscience and remote sensing;Disaster management;Emergency services;SAR;Buildings damage;Semantic change detection;Siamese network",
+        "number": "",
+        "pages": "6292-6295",
+        "title": "Assessing Buildings Damage from Multi-Temporal Sar Images Fusion using Semantic Change Detection",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2022"
+    },
+    "Lee2020AssessingPD": {
+        "author": "Jihyeon Lee and Joseph Z. Xu and Kihyuk Sohn and Wenhan Lu and David Berthelot and Izzeddin Gur and Pranav Khaitan and Ke Huang and Kyriacos M. Koupparis and Bernhard Kowatsch",
+        "journal": "ArXiv",
+        "title": "Assessing Post-Disaster Damage from Satellite Imagery using Semi-Supervised Learning Techniques",
+        "type": "article",
+        "url": "https://api.semanticscholar.org/CorpusID:227227614",
+        "volume": "abs/2011.14004",
+        "year": "2020"
+    },
+    "Weber2020BuildingDD": {
+        "author": "Ethan Weber and Hassan Kan{\\'e}",
+        "journal": "ArXiv",
+        "title": "Building Disaster Damage Assessment in Satellite Imagery with Multi-Temporal Fusion",
+        "type": "article",
+        "url": "https://api.semanticscholar.org/CorpusID:215745271",
+        "volume": "abs/2004.05525",
+        "year": "2020"
+    },
+    "Zhang2022SwinSUNetPT": {
+        "author": "Cui Zhang and Liejun Wang and Shuli Cheng and Yongming Li",
+        "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+        "pages": "1-1",
+        "title": "SwinSUNet: Pure Transformer Network for Remote Sensing Image Change Detection",
+        "type": "article",
+        "url": "https://api.semanticscholar.org/CorpusID:247509609",
+        "volume": "PP",
+        "year": "2022"
+    },
+    "rs13050905": {
+        "abstract": "The building damage status is vital to plan rescue and reconstruction after a disaster and is also hard to detect and judge its level. Most existing studies focus on binary classification, and the attention of the model is distracted. In this study, we proposed a Siamese neural network that can localize and classify damaged buildings at one time. The main parts of this network are a variety of attention U-Nets using different backbones. The attention mechanism enables the network to pay more attention to the effective features and channels, so as to reduce the impact of useless features. We train them using the xBD dataset, which is a large-scale dataset for the advancement of building damage assessment, and compare their result balanced F (F1) scores. The score demonstrates that the performance of SEresNeXt with an attention mechanism gives the best performance among single models, with the F1 score reaching 0.787. To improve the accuracy, we fused the results and got the best overall F1 score of 0.792. To verify the transferability and robustness of the model, we selected the dataset on the Maxar Open Data Program of two recent disasters to investigate the performance. By visual comparison, the results show that our model is robust and transferable.",
+        "article-number": "905",
+        "author": "Wu, Chuyi and Zhang, Feng and Xia, Junshi and Xu, Yichen and Li, Guoqing and Xie, Jibo and Du, Zhenhong and Liu, Renyi",
+        "doi": "10.3390/rs13050905",
+        "issn": "2072-4292",
+        "journal": "Remote Sensing",
+        "number": "5",
+        "title": "Building Damage Detection Using U-Net with Attention Mechanism from Pre- and Post-Disaster Remote Sensing Datasets",
+        "type": "Article",
+        "url": "https://www.mdpi.com/2072-4292/13/5/905",
+        "volume": "13",
+        "year": "2021"
+    }
+}});
